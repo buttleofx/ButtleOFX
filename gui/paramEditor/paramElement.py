e36e044185c7dd4a1739de0a12cb599f4daca3dc
agreffard <aurelien.greffard@gmail.com>
2012-12-01 18:32:53 +0100

    First dynamic ParamElements
    Display the default value of each elements created by python.

--- gui/paramEditor/paramElement.py
+++ gui/paramEditor/paramElement.py
@@ -4,33 +4,23 @@ from PySide import QtGui, QtDeclarative
 from paramInt import ParamInt
 from paramString import ParamString
 from wrappers.mainWrapper import MainWrapper
-from wrappers.intWrapper import IntWrapper
-from wrappers.stringWrapper import StringWrapper
 
 
 def main():
     app = QtGui.QApplication(sys.argv)
     view = QtDeclarative.QDeclarativeView()
 
-    paramListTmp = [
-            ParamInt(1.5, 5, 52),
-            ParamInt(defaultValue=2.5, minimum=5, maximum=52, text="truc oh oh oh"),
-            ParamInt(defaultValue=5.5, minimum=5, maximum=52),
-            ParamString(defaultValue="plop.jpg", stringType="filename"),
-            ParamInt(defaultValue=7.5, minimum=5, maximum=52),
-            ParamString(defaultValue="plop.jpg", stringType="filename")
+    paramList = [
+            ParamInt(60, 5, 128),
+            ParamInt(defaultValue=8, minimum=5, maximum=500, text="something"),
+            ParamInt(defaultValue=50, minimum=1, maximum=52, text=""),
+            ParamString(defaultValue="something.jpg", stringType="filename"),
+            ParamInt(defaultValue=7, minimum=5, maximum=12),
+            ParamString(defaultValue="somethingelse.jpg", stringType="filename")
     ]
 
-    mapTypeToWrapper = {
-        ParamInt: IntWrapper,
-        ParamString: StringWrapper
-    }
-
-    paramList = [mapTypeToWrapper[paramElt.__class__](paramElt) for paramElt in paramListTmp]
-    paramListModel = MainWrapper(view, paramList)
-
-    rc = view.rootContext()
-    rc.setContextProperty('_paramListModel', paramListModel)
+    mw = MainWrapper(view, paramList)
+    view.rootContext().setContextProperty('_paramListModel', mw)
 
     view.setSource('qml/paramElement.qml')
     view.setResizeMode(QtDeclarative.QDeclarativeView.SizeRootObjectToView)

1e2ed0b74f6c8907ed206ae3110038947fe33a97
Xopi <xflorimont@gmail.com>
2013-01-22 17:37:32 +0100

    Temporary replace label by identifier in getPlugins function

--- buttleofx/data/tuttleTools.py
+++ buttleofx/data/tuttleTools.py
@@ -13,9 +13,10 @@ def getPluginsNames():
     """
         Returns the list of all names of Tuttle's plugins.
     """
-    pluginCache = tuttle.core().getImageEffectPluginCache()
-    return [plugin.getDescriptor().getLabel() for plugin in pluginCache.getPlugins()]
-
+    #pluginCache = tuttle.core().getImageEffectPluginCache()
+    #return [plugin.getDescriptor().getLabel() for plugin in pluginCache.getPlugins()]
+    pluginCache = tuttle.core().getPluginCache()
+    return [plugin.getIdentifier() for plugin in pluginCache.getPlugins()]
 
 def getPlugin(pluginName):
     """

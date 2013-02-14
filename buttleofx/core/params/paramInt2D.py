606027fd2ff9039e6c871a9623a495721f4e2f4b
eprana <elisa.prana@free.fr>
2013-02-14 17:27:56 +0100

    Undo/redo on ParamInt2D

--- buttleofx/core/params/paramInt2D.py
+++ buttleofx/core/params/paramInt2D.py
@@ -1,15 +1,20 @@
 from quickmamba.patterns import Signal
+# undo redo
+from buttleofx.core.undo_redo.manageTools import CommandManager
+from buttleofx.core.undo_redo.commands.params import CmdSetParamInt2D
 
 
 class ParamInt2D(object):
     """
         Core class, which represents a int2D parameter.
-        Contains : 
+        Contains :
             - _tuttleParam : link to the corresponding tuttleParam
     """
 
     def __init__(self, tuttleParam):
         self._tuttleParam = tuttleParam
+        self._oldValue1 = self.getValue1()
+        self._oldValue2 = self.getValue2()
 
         self.changed = Signal()
 
@@ -27,8 +32,14 @@ class ParamInt2D(object):
     def getDefaultValue2(self):
         return self._tuttleParam.getProperties().getIntProperty("OfxParamPropDefault", 1)
 
-    def getValue(self):
+    def getValues(self):
         return (self.getValue1(), self.getValue2())
+
+    def getOldValue1(self):
+        return self._oldValue1
+
+    def getOldValue2(self):
+        return self._oldValue2
 
     def getValue1(self):
         return self._tuttleParam.getIntValueAtIndex(0)
@@ -53,13 +64,21 @@ class ParamInt2D(object):
 
     #################### setters ####################
 
-    def setValue(self, values):
+    def setValues(self, values):
         self.setValue1(values[0])
         self.setValue2(values[1])
+
+    def setOldValueAt(self, value, index):
+        if index == 0:
+            self._oldValue1 = value
+        else:
+            self._oldValue2 = value
 
     def setValue1(self, value):
         self._tuttleParam.setValueAtIndex(0, int(value))
         self.changed()
+
+        # Update Viewer
         from buttleofx.data import ButtleDataSingleton
         buttleData = ButtleDataSingleton().get()
         buttleData.updateMapAndViewer()
@@ -67,6 +86,18 @@ class ParamInt2D(object):
     def setValue2(self, value):
         self._tuttleParam.setValueAtIndex(1, int(value))
         self.changed()
+
+        # Update Viewer
         from buttleofx.data import ButtleDataSingleton
         buttleData = ButtleDataSingleton().get()
         buttleData.updateMapAndViewer()
+
+    def pushValue(self, newValue, index):
+        if index == 0:
+            cmdUpdate = CmdSetParamInt2D(self, (newValue, self.getValue2()), 0)
+            cmdManager = CommandManager()
+            cmdManager.push(cmdUpdate)
+        else:
+            cmdUpdate = CmdSetParamInt2D(self, (self.getValue1(), newValue), 1)
+            cmdManager = CommandManager()
+            cmdManager.push(cmdUpdate)

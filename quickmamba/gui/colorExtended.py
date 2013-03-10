33e045c75498f6d8a0dcfa14cb2b3ace03bd4ea6
Arthur <arthurtourneret@gmail.com>
2013-03-10 19:49:14 +0100

    implement getter and setter for brightness and saturation

--- quickmamba/gui/colorExtended.py
+++ quickmamba/gui/colorExtended.py
@@ -45,36 +45,83 @@ class ColorExtended(QtDeclarative.QDeclarativeItem):
         self._color.setHue(hue)
         self.colorChanged.emit()
 
+    #saturation
     def getSaturation(self):
-        return self._color.saturation()
+        print "GET SATURATION"
+        return self._color.saturationF()
 
-    def setSaturation(self, saturation):
-        self._color.setSaturation(saturation)
+    def setSaturation(self, saturationF):
+        print "SET SATURATION :", saturationF
+        # setValueF doesn't exist
+        hueF = self._color.hueF()
+        #print "hue : ", hueF
+        valueF = self._color.valueF()
+        #print "saturation : ", saturationF
+        alphaF = self._color.alphaF()
+        #print "alpha : ", alphaF
+        # I would use setHslF but for unknown reason (bug ?), it doesn't modify the saturation
+        self._color.setHsvF(hueF, saturationF, valueF, alphaF)
+        print "self._color.saturation :", self._color.saturationF()
+        #print "new ligthness : ", self._color.lightnessF()
         self.colorChanged.emit()
 
+    # brightness (corresponds to lightness for the moment)
     def getBrightness(self):
-        return self._color.brightness()
+        print "GET BRIGHTNESS"
+        return self._color.lightnessF()
 
-    def setBrightness(self, brightness):
-        self._color.setBrightness(brightness)
+    def setBrightness(self, brightnessF):
+        print "SET BRIGHTNESS"
+        # setValueF doesn't exist
+        hueF = self._color.hueF()
+        #print "hue : ", hueF
+        saturationF = self._color.saturationF()
+        #print "saturation : ", saturationF
+        alphaF = self._color.alphaF()
+        #print "alpha : ", alphaF
+        self._color.setHslF(hueF, saturationF, brightnessF, alphaF)
+        #print "new ligthness : ", self._color.lightnessF()
         self.colorChanged.emit()
 
+    # alpha
     def getAlpha(self):
         # alpha returns the value of alpha between 0 and 255 and alphaF between 0 and 1
         return self._color.alphaF()
 
-    def setAlpha(self, alpha):
-        self._color.setAlphaF(alpha)
+    def setAlpha(self, alphaF):
+        self._color.setAlphaF(alphaF)
+        self.colorChanged.emit()
+
+    # value (for v in hsva)
+    def getValue(self):
+        print "passed in getValue test ColorSlider", self._color.valueF()
+        return self._color.valueF()
+
+    def setValue(self, valueF):
+        # setValueF doesn't exist
+        hueF = self._color.hueF()
+        #print "hue : ", hueF
+        saturationF = self._color.saturationF()
+        #print "saturation : ", saturationF
+        alphaF = self._color.alphaF()
+        #print "alpha : ", alphaF
+        #print "value : ", self._color.valueF()
+        self._color.setHsvF(hueF, saturationF, valueF, alphaF)
+        #print "new value : ", self._color.valueF()
         self.colorChanged.emit()
 
     colorChanged = QtCore.Signal()
-    value = QtCore.Property(QtGui.QColor, getColor, setColor, notify=colorChanged)
+    #QColor (contains all information on the color)
+    entireColor = QtCore.Property(QtGui.QColor, getColor, setColor, notify=colorChanged)
+    #rgb
     red = QtCore.Property(float, getRed, setRed, notify=colorChanged)
     green = QtCore.Property(float, getGreen, setGreen, notify=colorChanged)
     blue = QtCore.Property(float, getBlue, setBlue, notify=colorChanged)
+    # hsva
     hue = QtCore.Property(float, getHue, setHue, notify=colorChanged)
     saturation = QtCore.Property(float, getSaturation, setSaturation, notify=colorChanged)
+    # brightness corresponds to lightness for the moment
     brightness = QtCore.Property(float, getBrightness, setBrightness, notify=colorChanged)
     alpha = QtCore.Property(float, getAlpha, setAlpha, notify=colorChanged)
-
+    value = QtCore.Property(float, getValue, setValue, notify=colorChanged)
 

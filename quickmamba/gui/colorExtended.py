02ad7addc79f2f4ab1a4f24f6ff714297124f5c3
Arthur <arthurtourneret@gmail.com>
2013-03-10 22:05:17 +0100

    changed some fonctions for manage colors

--- quickmamba/gui/colorExtended.py
+++ quickmamba/gui/colorExtended.py
@@ -5,11 +5,11 @@ from PySide import QtDeclarative
 class ColorExtended(QtDeclarative.QDeclarativeItem):
 
     _color = QtGui.QColor()
-	
+
     def __init__(self, parent=None):
         QtDeclarative.QDeclarativeItem.__init__(self, parent)
 
-
+    # the F at the end of a fonction a color.alphaF() means that the value returns or set is between 0 and 1
     def getColor(self):
         return self._color
 
@@ -18,110 +18,93 @@ class ColorExtended(QtDeclarative.QDeclarativeItem):
         self.colorChanged.emit()
 
     def getRed(self):
-        return self._color.red()
+        print "GET RED", self._color.redF()
+        return self._color.redF()
 
     def setRed(self, red):
-        self._color.setRed(red)
+        self._color.setRedF(red)
         self.colorChanged.emit()
 
     def getGreen(self):
-        return self._color.green()
+        return self._color.greenF()
 
     def setGreen(self, green):
-        self._color.setGreen(green)
+        self._color.setGreenF(green)
         self.colorChanged.emit()
 
     def getBlue(self):
-        return self._color.blue()
+        return self._color.blueF()
 
     def setBlue(self, blue):
-        self._color.setBlue(blue)
+        self._color.setBlueF(blue)
         self.colorChanged.emit()
 
+    # hue
     def getHue(self):
-        return self._color.hue()
+        return self._color.hueF()
 
-    def setHue(self, hue):
-        self._color.setHue(hue)
+    def setHue(self, hueF):
+        # setHueF doesn't exist
+        saturationF = self._color.saturationF()
+        print "saturation : ", saturationF
+        brightnessF = self._color.valueF()
+        print "brightness : ", brightnessF
+        alphaF = self._color.alphaF()
+        print "alpha : ", alphaF
+        self._color.setHsvF(hueF, saturationF, brightnessF, alphaF)
+        print "self._color.hue has changed :", self._color.hueF()
         self.colorChanged.emit()
 
-    #saturation
+    # saturation
     def getSaturation(self):
-        print "GET SATURATION"
         return self._color.saturationF()
 
     def setSaturation(self, saturationF):
-        print "SET SATURATION :", saturationF
-        # setValueF doesn't exist
+        # setSaturationF doesn't exist
         hueF = self._color.hueF()
-        #print "hue : ", hueF
-        valueF = self._color.valueF()
-        #print "saturation : ", saturationF
+        print "hue in setSaturation : ", hueF
+        brightnessF = self._color.valueF()
+        print "brightness : ", brightnessF
         alphaF = self._color.alphaF()
-        #print "alpha : ", alphaF
-        # I would use setHslF but for unknown reason (bug ?), it doesn't modify the saturation
-        self._color.setHsvF(hueF, saturationF, valueF, alphaF)
-        print "self._color.saturation :", self._color.saturationF()
-        #print "new ligthness : ", self._color.lightnessF()
+        print "alpha : ", alphaF
+        self._color.setHsvF(hueF, saturationF, brightnessF, alphaF)
+        print "self._color.saturation has changed :", self._color.saturationF()
         self.colorChanged.emit()
 
-    # brightness (corresponds to lightness for the moment)
+    # brightness is the b in hsb model for color (also named hsv, with v for value)
     def getBrightness(self):
-        print "GET BRIGHTNESS"
-        return self._color.lightnessF()
+        return self._color.valueF()
 
     def setBrightness(self, brightnessF):
-        print "SET BRIGHTNESS"
-        # setValueF doesn't exist
+        # setBrightnessF doesn't exist
         hueF = self._color.hueF()
-        #print "hue : ", hueF
+        print "hue in setBrightness : ", hueF
         saturationF = self._color.saturationF()
-        #print "saturation : ", saturationF
+        print "saturation : ", saturationF
         alphaF = self._color.alphaF()
-        #print "alpha : ", alphaF
-        self._color.setHslF(hueF, saturationF, brightnessF, alphaF)
-        #print "new ligthness : ", self._color.lightnessF()
+        print "alpha : ", alphaF
+        self._color.setHsvF(hueF, saturationF, brightnessF, alphaF)
+        print "new brightness : ", self._color.valueF()
         self.colorChanged.emit()
 
     # alpha
     def getAlpha(self):
-        # alpha returns the value of alpha between 0 and 255 and alphaF between 0 and 1
         return self._color.alphaF()
 
     def setAlpha(self, alphaF):
         self._color.setAlphaF(alphaF)
         self.colorChanged.emit()
 
-    # value (for v in hsva)
-    def getValue(self):
-        print "passed in getValue test ColorSlider", self._color.valueF()
-        return self._color.valueF()
-
-    def setValue(self, valueF):
-        # setValueF doesn't exist
-        hueF = self._color.hueF()
-        #print "hue : ", hueF
-        saturationF = self._color.saturationF()
-        #print "saturation : ", saturationF
-        alphaF = self._color.alphaF()
-        #print "alpha : ", alphaF
-        #print "value : ", self._color.valueF()
-        self._color.setHsvF(hueF, saturationF, valueF, alphaF)
-        #print "new value : ", self._color.valueF()
-        self.colorChanged.emit()
-
     colorChanged = QtCore.Signal()
-    #QColor (contains all information on the color)
+    # QColor (contains all information on the color)
     entireColor = QtCore.Property(QtGui.QColor, getColor, setColor, notify=colorChanged)
-    #rgb
+    # rgb (between 0 and 1)
     red = QtCore.Property(float, getRed, setRed, notify=colorChanged)
     green = QtCore.Property(float, getGreen, setGreen, notify=colorChanged)
     blue = QtCore.Property(float, getBlue, setBlue, notify=colorChanged)
-    # hsva
+    # hsba (also named hsva) (between 0 and 1)
     hue = QtCore.Property(float, getHue, setHue, notify=colorChanged)
     saturation = QtCore.Property(float, getSaturation, setSaturation, notify=colorChanged)
-    # brightness corresponds to lightness for the moment
     brightness = QtCore.Property(float, getBrightness, setBrightness, notify=colorChanged)
     alpha = QtCore.Property(float, getAlpha, setAlpha, notify=colorChanged)
-    value = QtCore.Property(float, getValue, setValue, notify=colorChanged)
 

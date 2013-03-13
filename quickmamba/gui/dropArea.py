072b272ba423589ff703a9a2e2e3bdc85c810446
agreffard <aurelien.greffard@gmail.com>
2013-03-13 20:08:17 +0100

    Modify events in Quickmamba to be able to manage them in QML
    The first thing to do is to emit the event

--- quickmamba/gui/dropArea.py
+++ quickmamba/gui/dropArea.py
@@ -44,139 +44,106 @@ class DropArea(QtDeclarative.QDeclarativeItem):
     def dragEnterEvent(self, event):
         #print 'dragEnterEvent'
 
-        if not self.acceptEvent(event):
-            event.setAccepted(False)
+        urls = event.mimeData().urls()
+        firstUrl = urls[0].toLocalFile() if len(urls) else ""
+        self.internDragEnter.emit(
+            event.mimeData().hasText(), event.mimeData().text(),
+            event.mimeData().hasHtml(), event.mimeData().html(),
+            event.mimeData().hasUrls(), firstUrl,
+            #event.mimeData().hasImage(), event.mimeData().imageData(),
+            #event.mimeData().hasColor(), event.mimeData().colorData(),
+            
+            event.buttons(), event.dropAction(),
+            event.modifiers(), event.pos(),
+            event.possibleActions(), event.proposedAction(),
+            "")
+            #event.source().accessibleName() if event.source() else "")
 
-        elif self._acceptDropValue:
-
-            #print 'dragEnterEvent acceptDrop'
-            event.setAccepted(True)
+        if self._acceptDropValue:
             event.acceptProposedAction()
             self.setCursor(QtCore.Qt.DragMoveCursor)
-            
-            urls = event.mimeData().urls()
-            firstUrl = urls[0].toLocalFile() if len(urls) else ""
-            self.internDragEnter.emit(
-                event.mimeData().hasText(), event.mimeData().text(),
-                event.mimeData().hasHtml(), event.mimeData().html(),
-                event.mimeData().hasUrls(), firstUrl,
-                #event.mimeData().hasImage(), event.mimeData().imageData(),
-                #event.mimeData().hasColor(), event.mimeData().colorData(),
-                
-                event.buttons(), event.dropAction(),
-                event.modifiers(), event.pos(),
-                event.possibleActions(), event.proposedAction(),
-                "")
-                #event.source().accessibleName() if event.source() else "")
+
+        event.setAccepted(self._acceptDropValue)
 
     def dragMoveEvent(self, event):
         #print 'dragMoveEvent'
 
-        if not self.acceptEvent(event):
-            event.setAccepted(False)
+        urls = event.mimeData().urls()
+        firstUrl = urls[0].toLocalFile() if len(urls) else ""
+        self.internDragMove.emit(
+            event.mimeData().hasText(), event.mimeData().text(),
+            event.mimeData().hasHtml(), event.mimeData().html(),
+            event.mimeData().hasUrls(), firstUrl,
+            #event.mimeData().hasImage(), event.mimeData().imageData(),
+            #event.mimeData().hasColor(), event.mimeData().colorData(),
 
-        elif self._acceptDropValue:
-            event.setAccepted(True)
+            event.buttons(), event.dropAction(),
+            event.modifiers(), event.pos(),
+            event.possibleActions(), event.proposedAction(),
+            "")
+            #event.source().accessibleName() if event.source() else "")
 
-            urls = event.mimeData().urls()
-            firstUrl = urls[0].toLocalFile() if len(urls) else ""
-            self.internDragMove.emit(
-                event.mimeData().hasText(), event.mimeData().text(),
-                event.mimeData().hasHtml(), event.mimeData().html(),
-                event.mimeData().hasUrls(), firstUrl,
-                #event.mimeData().hasImage(), event.mimeData().imageData(),
-                #event.mimeData().hasColor(), event.mimeData().colorData(),
-
-                event.buttons(), event.dropAction(),
-                event.modifiers(), event.pos(),
-                event.possibleActions(), event.proposedAction(),
-                "")
-                #event.source().accessibleName() if event.source() else "")
+        event.setAccepted(self._acceptDropValue)
 
     def dragLeaveEvent(self, event):
         #print 'dragLeaveEvent'
 
-        if not self.acceptEvent(event):
-            event.setAccepted(False)
-
-        elif self._acceptDropValue:
-            event.setAccepted(True)
-
-            urls = event.mimeData().urls()
-            firstUrl = urls[0].toLocalFile() if len(urls) else ""
-            self.internDragLeave.emit(
-                event.mimeData().hasText(), event.mimeData().text(),
-                event.mimeData().hasHtml(), event.mimeData().html(),
-                event.mimeData().hasUrls(), firstUrl,
-                #event.mimeData().hasImage(), event.mimeData().imageData(),
-                #event.mimeData().hasColor(), event.mimeData().colorData(),
-                
-                event.buttons(), event.dropAction(),
-                event.modifiers(), event.pos(),
-                event.possibleActions(), event.proposedAction(),
-                "")
-                #event.source().accessibleName() if event.source() else "")
+        urls = event.mimeData().urls()
+        firstUrl = urls[0].toLocalFile() if len(urls) else ""
+        self.internDragLeave.emit(
+            event.mimeData().hasText(), event.mimeData().text(),
+            event.mimeData().hasHtml(), event.mimeData().html(),
+            event.mimeData().hasUrls(), firstUrl,
+            #event.mimeData().hasImage(), event.mimeData().imageData(),
+            #event.mimeData().hasColor(), event.mimeData().colorData(),
             
-            self.unsetCursor()
+            event.buttons(), event.dropAction(),
+            event.modifiers(), event.pos(),
+            event.possibleActions(), event.proposedAction(),
+            "")
+            #event.source().accessibleName() if event.source() else "")
+        
+        event.setAccepted(self._acceptDropValue)
+        self.unsetCursor()
 
     def dropEvent(self, event):
         #print 'dropEvent'
 
-        if not self.acceptEvent(event):
-            event.setAccepted(False)
+        # hasText()  text()      text/plain
+        # hasHtml()  html()      text/html
+        # hasUrls()  urls()      text/uri-list
+        # hasImage() imageData() image/ *
+        # hasColor() colorData() application/x-color
+        
+        urls = event.mimeData().urls()
+        firstUrl = urls[0].toLocalFile() if len(urls) else ""
+
+        # creates a seg. fault at the end of the application,
+        # if event.source() is called.
+        #print "event source:", event.source()
+
+        self.internDrop.emit(
+            event.mimeData().hasText(), event.mimeData().text(),
+            event.mimeData().hasHtml(), event.mimeData().html(),
+            event.mimeData().hasUrls(), firstUrl,
+            #event.mimeData().hasImage(), event.mimeData().imageData(),
+            #event.mimeData().hasColor(), event.mimeData().colorData(),
+            
+            event.buttons(), event.dropAction(),
+            event.modifiers(), event.pos(),
+            event.possibleActions(), event.proposedAction(),
+            "")
+            #event.source().accessibleName() if event.source() else "")
+        
 
         if self._acceptDropValue:
-
-            event.setAccepted(True)
             event.acceptProposedAction()
             #print 'text:', event.mimeData().hasText(), event.mimeData().text()
             #print 'html:', event.mimeData().hasHtml(), event.mimeData().html()
             #print 'url:', event.mimeData().hasUrls(), [u.toLocalFile() for u in event.mimeData().urls()]
-            
-            # hasText()  text()      text/plain
-            # hasHtml()  html()      text/html
-            # hasUrls()  urls()      text/uri-list
-            # hasImage() imageData() image/ *
-            # hasColor() colorData() application/x-color
-            
-            urls = event.mimeData().urls()
-            firstUrl = urls[0].toLocalFile() if len(urls) else ""
 
-            # creates a seg. fault at the end of the application,
-            # if event.source() is called.
-            #print "event source:", event.source()
-
-            self.internDrop.emit(
-                event.mimeData().hasText(), event.mimeData().text(),
-                event.mimeData().hasHtml(), event.mimeData().html(),
-                event.mimeData().hasUrls(), firstUrl,
-                #event.mimeData().hasImage(), event.mimeData().imageData(),
-                #event.mimeData().hasColor(), event.mimeData().colorData(),
-                
-                event.buttons(), event.dropAction(),
-                event.modifiers(), event.pos(),
-                event.possibleActions(), event.proposedAction(),
-                "")
-                #event.source().accessibleName() if event.source() else "")
-            
-            self.unsetCursor()
-
-    def acceptEvent(self, event):
-        if (self._acceptedData == "mosquito" and event.mimeData().hasText() and event.mimeData().text() == "mosquito_of_the_dead") \
-        or (self._acceptedData == "connection" and event.mimeData().hasText() and event.mimeData().text().startswith("clip")) \
-        or (self._acceptedData == "urls" and event.mimeData().hasUrls()):
-            self.setAcceptDrop(True)
-            return True
-        else:
-            self.setAcceptDrop(False)
-            return False
-
-    def getAcceptedData(self):
-        return self._acceptedData
-
-    def setAcceptedData(self, data):
-        self._acceptedData = data
-        self.acceptedDataChanged.emit()
+        event.setAccepted(self._acceptDropValue)
+        self.unsetCursor()
 
     def getAcceptDrop(self):
         return self._acceptDropValue
@@ -187,10 +154,7 @@ class DropArea(QtDeclarative.QDeclarativeItem):
         self.acceptDropChanged.emit()
 
     acceptDropChanged = QtCore.Signal()
-    acceptedDataChanged = QtCore.Signal()
 
     _acceptDropValue = True
-    _acceptedData = "urls"
     acceptDrop = QtCore.Property(bool, getAcceptDrop, setAcceptDrop, notify=acceptDropChanged)
-    acceptedData = QtCore.Property(str, getAcceptedData, setAcceptedData, notify=acceptedDataChanged)
 

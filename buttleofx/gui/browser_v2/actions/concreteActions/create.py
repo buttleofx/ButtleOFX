8cfca8d3125fd822f2d4ea1bfb583b8cea28dfac
Mathias Goyheneche <mathias.goyheneche@gmail.com>
2015-01-21 16:09:15 +0100

    Create file and folder

---
+++ buttleofx/gui/browser_v2/actions/concreteActions/create.py
@@ -0,0 +1,37 @@
+import os
+import shutil
+
+from buttleofx.gui.browser_v2.actions.actionInterface import ActionInterface
+
+
+class Create(ActionInterface):
+
+    def __init__(self, parentBrowserItem, newBrowserItem):
+        # Parent must be a directory
+        if not parentBrowserItem.isFolder():
+            raise TypeError
+        super().__init__(parentBrowserItem)
+        # TODO: Can a new item be a sequence ?
+        self._newBrowserItem = newBrowserItem
+
+    def action(self):
+        parent = self.getBrowserItem()
+        newBrowserItem = self._newBrowserItem
+
+        # Create file
+        if newBrowserItem.isFile():
+            # TODO: Check parent's permission in try catch
+            parent_path = parent.getPath()
+            file_name = newBrowserItem.getName()
+            new_file_path = os.path.join(parent_path, file_name)
+            if os.path.exists(parent_path):
+                open(new_file_path, 'a').close()
+
+        # Create Folder
+        if newBrowserItem.isFolder():
+            # TODO: Check parent's permission in try catch
+            parent_path = parent.getPath()
+            folder_name = newBrowserItem.getName()
+            folder_path = os.path.join(parent_path, folder_name)
+            if os.path.exists(parent_path):
+                os.makedirs(folder_path)
---
+++ buttleofx/gui/browser_v2/actions/testConcreteActions/test_create.py
@@ -0,0 +1,66 @@
+import unittest
+import tempfile
+import os
+import shutil
+
+from OpenGL import GL
+
+from buttleofx.gui.browser_v2.browserItem import BrowserItem
+from buttleofx.gui.browser_v2.actions.concreteActions.create import Create
+
+
+class TestCreate(unittest.TestCase):
+
+    # Before tests run
+    def setUp(self):
+        pass
+
+    def test_file_create(self):
+        with tempfile.TemporaryDirectory() as path:
+            filename = 'new_file.txt'
+            parent_name = 'parent'
+            parent_path = os.path.join(path, parent_name)
+            file_path = os.path.join(parent_path, filename)
+
+            # Create parent folder
+            os.makedirs(parent_path)
+
+            # File should not exists
+            self.assertFalse(os.path.exists(file_path))
+
+            parent = BrowserItem(path, parent_name, 2, True)
+            new_file = BrowserItem(parent_path, filename, 1, True)
+
+            # Create file
+            cr = Create(parent, new_file)
+            cr.process()
+
+            # File should not exists
+            self.assertTrue(os.path.exists(file_path))
+
+    def test_folder_create(self):
+        with tempfile.TemporaryDirectory() as path:
+            folder_name = 'new_folder'
+            parent_name = 'parent'
+            parent_path = os.path.join(path, parent_name)
+            folder_path = os.path.join(parent_path, folder_name)
+
+            # Create parent folder
+            os.makedirs(parent_path)
+
+            # Folder should not exists
+            self.assertFalse(os.path.exists(folder_path))
+
+            parent = BrowserItem(path, parent_name, 2, True)
+            new_folder = BrowserItem(parent_path, folder_name, 2, True)
+
+            # Create folder
+            cr = Create(parent, new_folder)
+            cr.process()
+
+            # Folder should not exists
+            self.assertTrue(os.path.exists(folder_path))
+
+    # After tests run
+    def tearDown(self):
+        pass

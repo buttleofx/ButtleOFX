d8b752055a7629ec7b2fa9e3fd856b899d222b4c
Mathias Goyheneche <mathias.goyheneche@gmail.com>
2015-01-21 11:44:52 +0100

    temp directory instead of home directory

--- buttleofx/gui/browser_v2/actions/testConcreteActions/test_delete.py
+++ buttleofx/gui/browser_v2/actions/testConcreteActions/test_delete.py
@@ -1,8 +1,7 @@
 import unittest
+import tempfile
 import os
 import shutil
-
-import inspect
 
 from OpenGL import GL
 
@@ -11,65 +10,62 @@ from buttleofx.gui.browser_v2.actions.concreteActions.delete import Delete
 
 
 class TestDelete(unittest.TestCase):
+
     # Before tests run
     def setUp(self):
-        self._directory = "buttle_test_delete"
-        self._base_path = os.path.expanduser("~")
-        self._path = os.path.join(self._base_path, self._directory)
-        if os.path.exists(self._path):
-            shutil.rmtree(self._path)
-        os.makedirs(self._path)
+        pass
 
     def test_file_delete(self):
-        filename = 'delete_me.txt'
-        file_path = os.path.join(self._path, filename)
-        if os.path.exists(file_path):
-            os.remove(file_path)
+        with tempfile.TemporaryDirectory() as path:
+            filename = 'delete_me.txt'
+            file_path = os.path.join(path, filename)
+            if os.path.exists(file_path):
+                os.remove(file_path)
 
-        # File should not exists
-        self.assertFalse(os.path.exists(file_path))
+            # File should not exists
+            self.assertFalse(os.path.exists(file_path))
 
-        # Create file
-        open(file_path, 'a').close()
+            # Create file
+            open(file_path, 'a').close()
 
-        # File should exists
-        self.assertTrue(os.path.exists(file_path))
+            # File should exists
+            self.assertTrue(os.path.exists(file_path))
 
-        # Delete file
-        bi = BrowserItem(self._path, filename, 1, True)
+            # Delete file
+            bi = BrowserItem(path, filename, 1, True)
 
-        # Delete folder
-        de = Delete(bi)
-        de.process()
+            # Delete folder
+            de = Delete(bi)
+            de.process()
 
-        # File should not exists
-        self.assertFalse(os.path.exists(file_path))
+            # File should not exists
+            self.assertFalse(os.path.exists(file_path))
 
     def test_folder_delete(self):
-        folder_name = 'delete_me'
-        folder_path = os.path.join(self._path, folder_name)
-        if os.path.exists(folder_path):
-            shutil.rmtree(folder_path)
+        with tempfile.TemporaryDirectory() as path:
+            folder_name = 'delete_me'
+            folder_path = os.path.join(path, folder_name)
+            if os.path.exists(folder_path):
+                shutil.rmtree(folder_path)
 
-        # Folder should not exists
-        self.assertFalse(os.path.exists(folder_path))
+            # Folder should not exists
+            self.assertFalse(os.path.exists(folder_path))
 
-        # Create folder
-        os.makedirs(folder_path)
+            # Create folder
+            os.makedirs(folder_path)
 
-        # Folder should exists
-        self.assertTrue(os.path.exists(folder_path))
+            # Folder should exists
+            self.assertTrue(os.path.exists(folder_path))
 
-        bi = BrowserItem(self._path, folder_name, 2, True)
-        # Delete folder
-        de = Delete(bi)
-        de.process()
+            bi = BrowserItem(path, folder_name, 2, True)
+            # Delete folder
+            de = Delete(bi)
+            de.process()
 
-        # Folder should not exists
-        self.assertFalse(os.path.exists(folder_path))
+            # Folder should not exists
+            self.assertFalse(os.path.exists(folder_path))
 
     # After tests run
     def tearDown(self):
-        if os.path.exists(self._path):
-            shutil.rmtree(self._path)
+        pass
 

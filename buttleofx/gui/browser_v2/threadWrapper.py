e160325b161abc3b18f2dee9487f32206a1074b3
Alex <alexis.oblet@gmail.com>
2015-01-23 22:13:30 +0100

    ThreadWrapper: update structure
    for binding arguments

--- buttleofx/gui/browser_v2/threadWrapper.py
+++ buttleofx/gui/browser_v2/threadWrapper.py
@@ -10,10 +10,10 @@ class ThreadWrapper():
     def getLock(self):
         return self._lock
 
-    def startThread(self, f):
+    def startThread(self, f, argsParam=()):
         if len(self._jobs) > 20:
             return
-        self._jobs.append(threading.Thread(target=f))
+        self._jobs.append(threading.Thread(target=f, args=argsParam))
         self._jobs[-1:][0].start()
 
     def getNbJobs(self):
@@ -21,3 +21,6 @@ class ThreadWrapper():
 
     def pop(self):
         self._jobs.pop(0)
+
+    def stopAllThreads(self):
+        self._jobs.clear()

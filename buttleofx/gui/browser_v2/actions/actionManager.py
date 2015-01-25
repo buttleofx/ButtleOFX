5f2096c89cedd23d4858f844b489d1be12bad0ad
Alex <alexis.oblet@gmail.com>
2015-01-25 20:30:12 +0100

    modify ActionManager for workers

--- buttleofx/gui/browser_v2/actions/actionManager.py
+++ buttleofx/gui/browser_v2/actions/actionManager.py
@@ -1,54 +1,56 @@
 from buttleofx.gui.browser_v2.actions.actionInterface import ActionInterface
-from PyQt5 import QtCore
+from buttleofx.gui.browser_v2.actions.worker import Worker
 from quickmamba.patterns import Singleton
+from PyQt5 import QtCore
+from queue import Queue
 
 
 class ActionManager(QtCore.QObject):
     """
-        Handle the requested actions on browserItem(s) by a pile FIFO.
+        Handle the requested actions on browserItem(s) by a Queue FIFO.
         A second pile 'actionsEnded' is used for history
     """
 
     actionChanged = QtCore.pyqtSignal()
+    num_threads_workers = 5
 
     def __init__(self):
         super(ActionManager, self).__init__()
-        self._actionsEnded = []
-        self._actionsRunning = []
+        self._waiting = Queue(maxsize=0)  #
+        self._running = []
+        self._ended = []
+        self._workers = [Worker(self._waiting, self._running, self._ended, self.actionChanged) for _ in range(self.num_threads_workers)]
+        self.startWorkers()
 
-    def pull(self):
-        if not self._actionsRunning:
-            return
-        self._actionsRunning[0].process()
-        self._actionsEnded.append(self._actionsRunning.pop(0))
-        self.actionChanged.emit()
+    def startWorkers(self):
+        for worker in self._workers:
+            worker.start()
 
     def push(self, action):
-        self._actionsRunning.append(action)
+        self._waiting.put(action)
         self.actionChanged.emit()
 
     def clearEndedActions(self):
-        self._actionsEnded.clear()
+        self._ended.clear()
+        self.actionChanged.emit()
+
+    def clearRunningActions(self):
+        self._running.clear()
         self.actionChanged.emit()
 
     # ################################### Methods exposed also to QML ############################### #
 
     @QtCore.pyqtSlot(result=list)
     def getEndedActions(self):
-        return self._actionsEnded
+        return self._ended
 
     @QtCore.pyqtSlot(result=list)
     def getRunningActions(self):
-        return self._actionsRunning
+        return self._running
 
-    @QtCore.pyqtSlot(int)
-    def removeAt(self, i):
-        if i not in len(self._actionsRunning):
-            raise IndexError("ActionManager::removeAt(i) bad index")
-
-        self._actionsRunning[i].abort()
-        self._actionsEnded.append(self._actionsRunning.pop(i))
-        self.actionChanged.emit()
+    @QtCore.pyqtSlot(result=list)
+    def getWaitingActions(self):
+        return self._waiting
 
 
 class ActionManagerSingleton(Singleton):

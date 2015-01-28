936967ec807e5ce7537e0ec03763cecbbc7c387f
Mathias Goyheneche <mathias.goyheneche@gmail.com>
2015-01-28 19:19:35 +0100

    update delete action

--- buttleofx/gui/browser_v2/actions/concreteActions/delete.py
+++ buttleofx/gui/browser_v2/actions/concreteActions/delete.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import tempfile
 
 from buttleofx.gui.browser_v2.actions.actionInterface import ActionInterface
 
@@ -8,39 +9,51 @@ class Delete(ActionInterface):
 
     def __init__(self, browserItem):
         super().__init__(browserItem)
+        self._tmp = tempfile.TemporaryDirectory()
+        self._destItems = []
 
     def execute(self):
         browserItem = self.getBrowserItem()
 
         # Delete file
-        if browserItem.isFile():
+        # if browserItem.isFile():
             # TODO: Check permission in try catch
-            browserItemPath = browserItem.getPath()
-            if os.path.exists(browserItemPath):
-                os.remove(browserItemPath)
+            # browserItemPath = browserItem.getPath()
+            # if os.path.exists(browserItemPath):
+                # os.remove(browserItemPath)
 
         # Delete folder
-        if browserItem.isFolder():
+        if browserItem.isFile() or browserItem.isFolder():
             # TODO: Check permission in try catch
             browserItemPath = browserItem.getPath()
             if os.path.exists(browserItemPath):
-                shutil.rmtree(browserItemPath)
+                # shutil.rmtree(browserItemPath)
+                shutil.move(browserItem.getPath(), self._tmp.name)
+                dest_path = os.path.join(self._tmp.name, browserItem.getName())
+                self._destItems.append(dest_path)
 
         # Delete sequence
         if browserItem.isSequence():
-            pass
-            print("TODO: Delete sequence")
-            # print(dir(browserItem))
-            # print(browserItem.getPath())
-            # print(dir(browserItem.getSequence()._sequence))
-            # print(browserItem.getSequence()._sequence.getFilenameAt(0))
-            # print(browserItem.getSequence()._sequence.getFirstFilename())
-            # print(dir(browserItem.getSequence()._sequence.getFramesIterable()))
-            # print(browserItem.getSequence()._sequence.getFiles())
-            # i = 0
-            # for f in browserItem.getSequence()._sequence.getFramesIterable():
-                # print(i)
-                # print(dir(f))
-            # print(browserItem.getSequence())
+            seqParsed = browserItem.getSequence().getSequenceParsed()
+            frames = seqParsed.getFramesIterable()
+
+            for f in frames:
+                filename = seqParsed.getFilenameAt(f)
+                filePath = os.path.join(browserItem.getParentPath(), filename)
+                shutil.move(filePath, self._tmp.name)
+                self._destItems.append(os.path.join(self._tmp.name, filename))
+
+    def revert(self):
+        browserItem = self._browserItem
+
+        if browserItem.isFile() or browserItem.isFolder():
+            shutil.move(self._destItems[0], browserItem.getParentPath())
+
+        if browserItem.isSequence():
+            for item in self._destItems:
+                shutil.move(item, browserItem.getParentPath())
 
 
+    def __del__(self):
+        self._tmp.cleanup()
+        pass
--- buttleofx/gui/browser_v2/actions/testConcreteActions/test_delete.py
+++ buttleofx/gui/browser_v2/actions/testConcreteActions/test_delete.py
@@ -43,6 +43,36 @@ class TestDelete(unittest.TestCase):
             # File should not exists
             self.assertFalse(os.path.exists(file_path))
 
+    def test_file_delete_revert(self):
+        with tempfile.TemporaryDirectory() as path:
+            filename = 'delete_me.txt'
+            file_path = os.path.join(path, filename)
+            if os.path.exists(file_path):
+                os.remove(file_path)
+
+            # File should not exists
+            self.assertFalse(os.path.exists(file_path))
+
+            # Create file
+            open(file_path, 'a').close()
+
+            # File should exists
+            self.assertTrue(os.path.exists(file_path))
+            sp_file = sequenceParser.Item(sequenceParser.eTypeFile,
+                                          file_path)
+            bi = BrowserItem(sp_file, True)
+
+            # Delete file
+            de = Delete(bi)
+            de.process()
+
+            # File should not exists
+            self.assertFalse(os.path.exists(file_path))
+
+            de.revert()
+
+            self.assertEqual(len(sequenceParser.browse(path)), 1)
+
     def test_folder_delete(self):
         with tempfile.TemporaryDirectory() as path:
             folder_name = 'delete_me'
@@ -69,6 +99,36 @@ class TestDelete(unittest.TestCase):
             # Folder should not exists
             self.assertFalse(os.path.exists(folder_path))
 
+    def test_folder_delete_revert(self):
+        with tempfile.TemporaryDirectory() as path:
+            folder_name = 'delete_me'
+            folder_path = os.path.join(path, folder_name)
+            if os.path.exists(folder_path):
+                shutil.rmtree(folder_path)
+
+            # Folder should not exists
+            self.assertFalse(os.path.exists(folder_path))
+
+            # Create folder
+            os.makedirs(folder_path)
+
+            # Folder should exists
+            self.assertTrue(os.path.exists(folder_path))
+            sp_folder = sequenceParser.Item(sequenceParser.eTypeFolder,
+                                            folder_path)
+            bi = BrowserItem(sp_folder, True)
+
+            # Delete folder
+            de = Delete(bi)
+            de.process()
+
+            # Folder should not exists
+            self.assertFalse(os.path.exists(folder_path))
+
+            de.revert()
+
+            self.assertEqual(len(sequenceParser.browse(path)), 1)
+
     def test_sequence_delete(self):
         with tempfile.TemporaryDirectory() as path:
             # Create Sequence
@@ -83,34 +143,27 @@ class TestDelete(unittest.TestCase):
             de.process()
 
             # Sequence should not exists
-            self.assertFalse(True)
+            self.assertEqual(len(sequenceParser.browse(path)), 0)
 
+    def test_sequence_delete_revert(self):
+        with tempfile.TemporaryDirectory() as path:
+            # Create Sequence
+            h.create_sequence(path)
+            # Create BrowserItem sequence
+            sp_seq = sequenceParser.browse(path)[0]
 
-            # self.assertFalse(True)
-            #
-            # folder_name = ''
-            # folder_path = os.path.join(path, folder_name)
-            # if os.path.exists(folder_path):
-            #     shutil.rmtree(folder_path)
-            #
-            # # Folder should not exists
-            # self.assertFalse(os.path.exists(folder_path))
-            #
-            # # Create folder
-            # os.makedirs(folder_path)
-            #
-            # # Folder should exists
-            # self.assertTrue(os.path.exists(folder_path))
-            # sp_folder = sequenceParser.Item(sequenceParser.eTypeFolder,
-            #                                 folder_path)
-            # bi = BrowserItem(sp_folder, True)
-            #
-            # # Delete folder
-            # de = Delete(bi)
-            # de.process()
-            #
-            # # Folder should not exists
-            # self.assertFalse(os.path.exists(folder_path))
+            bi = BrowserItem(sp_seq, True)
+            self.assertIsNotNone(bi.getName())
+            # Delete sequence
+            de = Delete(bi)
+            de.process()
+
+            # Sequence should not exists
+            self.assertEqual(len(sequenceParser.browse(path)), 0)
+
+            de.revert()
+
+            self.assertEqual(len(sequenceParser.browse(path)), 1)
 
     # After tests run
     def tearDown(self):

e4838462a37ee6554e0d1f2c8744ce60d1ae1b39
Alex <alexis.oblet@gmail.com>
2015-02-05 22:40:50 +0100

    Browser fix issue: #136 completion + add filter in url
    for the moment, have to press enter key to validate
    ie: /home/toto/img/*.jpg (&& enter)

--- buttleofx/gui/browser_v2/browserModel.py
+++ buttleofx/gui/browser_v2/browserModel.py
@@ -186,6 +186,7 @@ class BrowserModel(QtCore.QObject):
         self._filter = newFilter
         self.updateItemsWrapperAsync()
         self.filterChanged.emit()
+        self.modelChanged.emit()
 
     def getCurrentPath(self):
         return self._currentPath
@@ -300,12 +301,12 @@ class BrowserModel(QtCore.QObject):
             return QObjectListModel(self)
 
         if not os.path.exists(path):
-            nameFiltering = os.path.basename(path)
+            nameFiltering = os.path.basename(path).lower()
             modelToNav = BrowserModel(asyncMode=False, path=os.path.dirname(path))  # need to be sync
 
         # get dirs with filtering if exists
         if nameFiltering:
-            tmpList = [item for item in modelToNav.getItems() if item.isFolder() and nameFiltering in item.getName()]
+            tmpList = [item for item in modelToNav.getItems() if item.isFolder() and nameFiltering in item.getName().lower()]
         else:
             tmpList = [item for item in modelToNav.getItems() if item.isFolder()]
 
@@ -367,6 +368,7 @@ class BrowserModel(QtCore.QObject):
     fileItems = QtCore.pyqtProperty(QtCore.QObject, getItems, notify=modelChanged)
     parentFolder = QtCore.pyqtProperty(str, getParentPath, notify=currentPathChanged)
     showSequence = QtCore.pyqtProperty(bool, isShowSequence, setShowSequence, notify=filterChanged)
+    filter = QtCore.pyqtProperty(str, getFilter, setFilter, notify=filterChanged)
 
     hideDotFiles = QtCore.pyqtProperty(bool, isHiddenDotFiles, setHideDotFiles, notify=hideDotFilesChanged)
     splittedCurrentPath = QtCore.pyqtProperty(QObjectListModel, getSplittedCurrentPath, notify=currentPathChanged)

65ce9723a262d7a4c30204513005c4d95eacdb9b
Mathias Goyheneche <mathias.goyheneche@gmail.com>
2015-02-14 14:15:56 +0100

    remove dest browser item param in copy

--- buttleofx/gui/browser_v2/actions/browserAction.py
+++ buttleofx/gui/browser_v2/actions/browserAction.py
@@ -1,26 +1,102 @@
 from PyQt5 import QtCore
 from buttleofx.gui.browser_v2.actions.actionManager import ActionManagerSingleton
 from buttleofx.gui.browser_v2.actions.actionWrapper import ActionWrapper
+from quickmamba.patterns.singleton import Singleton
+from buttleofx.gui.browser_v2.browserModel import BrowserModelSingleton
+from buttleofx.gui.browser_v2.browserItem import BrowserItem
+from buttleofx.gui.browser_v2.actions.concreteActions.copy import Copy
+from buttleofx.gui.browser_v2.actions.concreteActions.move import Move
+from buttleofx.gui.browser_v2.actions.concreteActions.create import Create
+from buttleofx.gui.browser_v2.actions.concreteActions.delete import Delete
+from pySequenceParser import sequenceParser
+import os
 
 
 class BrowserAction(QtCore.QObject):
     """
         Controller of browser for actions
     """
+    cacheChanged = QtCore.pyqtSignal()
+
     def __init__(self):
         super(BrowserAction, self).__init__()
         self._cacheActions = None  # for copy, move actions
-
-    def getCache(self):
-        return self._cacheActions
+        self._browserModel = BrowserModelSingleton.get()
 
     def pushCache(self, listActions):
         self._cacheActions = ActionWrapper(listActions)
 
+    def isEmptyCache(self):
+        return bool(self._cacheActions)
+
+    @QtCore.pyqtSlot(QtCore.QObject)
+    def getCache(self):
+        return self._cacheActions
+
+    @QtCore.pyqtSlot(QtCore.QObject)
     def pushToActionManager(self, actionWrapper=None):
         if actionWrapper:
             ActionManagerSingleton.get().push(actionWrapper)
         else:
             if self._cacheActions:
                 ActionManagerSingleton.get().push(self._cacheActions)
-            self._cacheActions = None
+
+    @QtCore.pyqtSlot()
+    def handleCopy(self):
+        self._cacheActions = None
+        listActions = []
+        for bItem in self._browserModel.getItems():
+            if bItem.getSelected():
+                listActions.append(Copy(bItem))
+        self.pushCache(listActions)
+        self.cacheChanged.emit()
+
+    @QtCore.pyqtSlot()
+    def handleMove(self):
+        self._cacheActions = None
+        listActions = []
+        for bItem in self._browserModel.getItems():
+            if bItem.getSelected():
+                listActions.append(Move(bItem))
+        self.pushCache(listActions)
+        self.cacheChanged.emit()
+
+    @QtCore.pyqtSlot(str)
+    def handlePaste(self, destination):
+        destination = destination.strip() if destination.strip() else self._browserModel.getCurrentPath()
+        if not self._cacheActions or not destination or not os.path.exists(destination):
+            return
+
+        for action in self._cacheActions.getActions():
+            action.setDestinationPath(destination)
+        self.pushToActionManager()
+
+    @QtCore.pyqtSlot()
+    def handleDelete(self):
+        listActions = []
+        for item in [bItem for bItem in self._browserModel.getItems() if bItem.getSelected()]:
+            listActions.append(Delete(item))
+        self.pushToActionManager(ActionWrapper(listActions))
+
+    @QtCore.pyqtSlot(str)
+    def handleNew(self, type):
+        if type == "Folder":
+            new = BrowserItem(sequenceParser.Item(sequenceParser.eTypeFolder, "New_Folder"), True)
+        elif type == "File":
+            new = BrowserItem(sequenceParser.Item(sequenceParser.eTypeFile, "NewDocument.txt"), True)
+        else:
+            return
+
+        parent = BrowserItem(sequenceParser.Item(sequenceParser.eTypeFolder, self._browserModel.getCurrentPath()), False)
+        self.pushToActionManager(ActionWrapper([Create(parent, new)]))
+
+    # cache empty ?
+    isCache = QtCore.pyqtProperty(bool, isEmptyCache, notify=cacheChanged)
+
+
+class BrowserActionSingleton(Singleton):
+    _bAction = BrowserAction()
+
+    @staticmethod
+    def get():
+        return BrowserActionSingleton._bAction

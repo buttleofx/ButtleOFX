520444b653084f88bd9c0d098470040b89aa2530
mxmgilbert <mxm.gilbert@gmail.com>
2015-02-14 11:57:59 +0100

    clean code

--- quickmamba/patterns/__init__.py
+++
@@ -1,3 +0,0 @@
-from .singleton import Singleton
-from .signalEvent import Signal
-
--- quickmamba/patterns/singleton.py
+++
@@ -1,8 +0,0 @@
-
-class Singleton(object):
-    def __new__(type):
-        if not '_the_instance' in type.__dict__:
-            type._the_instance = object.__new__(type)
-        return type._the_instance
-
-

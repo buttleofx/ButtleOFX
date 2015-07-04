b4547ca8268561bd6eb3f8e87e64715c1fb6dfb3
Alex <alexis.oblet@gmail.com>
2015-07-04 13:26:27 +0200

    clean actionWorker

--- buttleofx/gui/browser_v2/actions/actionWorker.py
+++ buttleofx/gui/browser_v2/actions/actionWorker.py
@@ -18,23 +18,34 @@ class ActionWorker(threading.Thread):
         logging.debug('ActionWorker destructor')
 
     def executeActionWrapper(self, actionWrapper):
+        """
+            - Change the actions status (ie move to other list, waiting->running->ended)
+            - Execute actions wrapped by actionWrapper
+            - Emit signal, actionChanged status
+        """
         self._actionManager.getRunningActions().append(actionWrapper)
         self._actionManager.actionChanged.emit()
+
         actionWrapper.executeActions()
         self._actionManager.actionChanged.emit()
+
         self._actionManager.getRunningActions().remove(actionWrapper)
         self._actionManager.getEndedActions().append(actionWrapper)
+        self._actionManager.actionChanged.emit()
 
     def run(self):
         while True:
             try:
-                # logging.debug('ActionWorker queue get')
-                actionWrapper = self._actionManager.getWaitingActionsQueue().get(timeout=1)
+                logging.debug('ActionWorker queue get')
+
+                # blocks until an action is submitted
+                actionWrapper = self._actionManager.getWaitingActionsQueue().get()
+
                 # stop the worker if we meet an end-of-queue markers
                 if not actionWrapper:
                     logging.debug('ActionWorker end-of-queue markers')
                     return
                 self.executeActionWrapper(actionWrapper)
             except queue.Empty:
-                # logging.debug('ActionWorker queue empty')
+                logging.debug('ActionWorker queue empty')
                 pass
